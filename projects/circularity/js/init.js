4942d9fefd22866f7d040621fde2cdb0440cb814
Bria Lawrence <bria.lawrence@jpschools.org>
2024-11-14 17:35:42 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,14 +20,21 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle(){
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+physikz.addRandomVelocity(circle, canvas);
+view.addChild(circle);
+circles.push(circle);
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
-
+        for (var loopsCompleted = 0; loopsCompleted < 100; loopsCompleted++){
+drawCircle();
+        }
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -38,14 +45,17 @@ var init = function (window) {
         and check to see if it has drifted off the screen.         
         */
         function update() {
+            
             // TODO 4 : Update the circle's position //
-
+            //was deteted because I was told to do so !//
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
-
+             //was deteted because I was told to do so !//
             // TODO 9 : Iterate over the array
-           
+            for (var i = 0; i < circles.length; i++){
+                physikz.updatePosition(circles[i]);
+                game.checkCirclePosition(circles[i]);
+            }
             
         }
     
@@ -62,8 +72,18 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
-
+            //if the circle goes off the left side, reset its x-coordinate to the right edge//
+            if (circle.x < 0) {
+                circle.x = canvas.width;
+            }
+//if the circle goes off the top side, reset its y-coordinate to the bottom edge//
+ if (circle.y < 0) {
+    circle.y = canvas.height;
+}
+//if the circle goes off the bottom side, reset its y-coordinate to the top edge//
+ if (circle.y > canvas.height) {
+    circle.y = 0;
+}
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
